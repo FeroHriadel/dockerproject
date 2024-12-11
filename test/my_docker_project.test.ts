98ee25395caa8b59f08b8debb51e36942e11739b
Fero Hriadel <ferdinand.hriadel@gmail.com>
2024-12-11 09:02:11 +0100

    Initial commit

---
+++ test/my_docker_project.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MyDockerProject from '../lib/my_docker_project-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/my_docker_project-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MyDockerProject.MyDockerProjectStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
